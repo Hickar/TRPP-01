17fcc03c5a156e141e3e7c13166650e6773e6078
Hickar <hickar@icloud.com>
2021-03-09 19:49:37 +0300

    Merge

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 window.addEventListener("load", () => {
-  console.log("Script has been loaded! Success!");
+  // console.log("Script has been loaded! Success!");
+  console.log("new commit changes");
 })
